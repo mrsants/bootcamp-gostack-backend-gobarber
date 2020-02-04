8bec47fe72561073cfcbedb46fa538a4eb203764
Marcelo <59406899+mrsants@users.noreply.github.com>
2020-02-03 22:38:04 -0300

    Implementacao de paginacao e listagem de agenda do prestador

--- src/app/controllers/AppointmentsController.js
+++ src/app/controllers/AppointmentsController.js
@@ -1,8 +1,37 @@
-import Appointments from '../models/Appointments';
+import { isBefore, parseISO, startOfHour } from 'date-fns';
 import * as Yup from 'yup';
+import Appointments from '../models/Appointments';
+import File from '../models/File';
 import User from '../models/User';
 
 class AppointmentsController {
+  async index(req, res) {
+    const { page = 1 } = req.query;
+
+    const appointments = await Appointments.findAll({
+      where: { user_id: req.userId, canceled_at: null },
+      order: ['date'],
+      attributes: ['id', 'date'],
+      limit: 20,
+      offset: (page -1 ) * 20,
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['id', 'name'],
+          include: [
+            {
+              model: File,
+              as: 'avatar',
+              attributes: ['id', 'path', 'url'],
+            },
+          ],
+        },
+      ],
+    });
+
+    return res.json(appointments);
+  }
   /**
    * @method store
    * @param {*} req
@@ -24,23 +53,59 @@ class AppointmentsController {
     /**
      * Check if provider_id is a provider
      */
-    const isProvider = await User.findOne({
+    const checkisProvider = await User.findOne({
       where: {
         id: provider_id,
         provider: true,
       },
     });
 
-    if (!isProvider) {
+    console.log(checkisProvider);
+
+    if (!checkisProvider) {
       return res.status(401).json({
         error: 'You can only create appointments with providers',
       });
     }
 
+    /**
+     * Check for past dates
+     */
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({
+        error: 'Past dates are not permitted',
+      });
+    }
+
+    /**
+     * Check date availability
+     */
+    const checkAvailability = await Appointments.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (checkAvailability) {
+      return res.status(400).json({
+        error: 'Appointment date is not available',
+      });
+    }
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({
+        error: 'Past dates are not permitted',
+      });
+    }
+
     const appointments = await Appointments.create({
-      user_id: req.user_id,
+      user_id: req.userId,
       provider_id,
-      date,
+      date: hourStart,
     });
 
     return res.status(200).json(appointments);
--- src/routes.js
+++ src/routes.js
@@ -7,15 +7,20 @@ import multerConfig from './config/multerConfig';
 import AuthMiddleware from './middleware/auth';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentsController from './app/controllers/AppointmentsController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
 routes.use(AuthMiddleware);
 routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
 routes.post('/files', upload.single('file'), FileController.store);
+routes.get('/appointments', AppointmentsController.index);
 routes.post('/appointments', AppointmentsController.store);
+routes.get('/schedule', ScheduleController.index);
+
 export default routes;
